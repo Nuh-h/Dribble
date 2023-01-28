85902084c90fe45e23c2b10be70cf326a1d03ac4
Nuh <69435206+Nuh-h@users.noreply.github.com>
2023-01-28 08:28:39 +0000

    [*] Cleans up content script

--- content.js
+++ content.js
@@ -1,13 +1,10 @@
 
 let rulesForThisSite;
 
-//Ask background js for updated store
 const site = new URL(document.location);
 
 chrome.runtime.sendMessage({ type: "GET_RULES", site: site.origin }, (response) => {
     const { data } = response ?? {};
-
-    console.log("Current site is: ", site)
 
     const rules = data;
 
@@ -26,92 +23,88 @@ chrome.runtime.sendMessage({ type: "GET_RULES", site: site.origin }, (response) 
 
 window.addEventListener('scroll', async () => {
 
-    const pathname = window.location.pathname;
-
     chrome.runtime.sendMessage({ type: "UPDATE_BADGE", value: rulesForThisSite?.length ?? 0 });
-    console.log({ rulesForThisSite })
 
-    if (rulesForThisSite) {
+    rulesForThisSite?.forEach(rule => {
 
-        rulesForThisSite.forEach(rule => {
+        rule?.itemsSelectors?.forEach(container => {
 
-            rule?.itemsSelectors?.forEach(container => {
+            const cards = document.querySelectorAll(container);
 
-                const cards = document.querySelectorAll(container);
+            cards.every(card => {
+                const text = card.innerText;
 
-                cards.forEach(card => {
-                    const text = card.innerText;
+                const checkForTerms = (text, terms) => {
+                    let isTermFound = false;
 
-                    const checkForTerms = (text, terms) => {
-                        let isTermFound = false;
+                    terms?.every(term => {
+                        const pattern = new RegExp(term, "gi")
+                        isTermFound = text.toLowerCase().split(pattern).length > 1;
 
-                        terms.forEach(term => {
-                            const pattern = new RegExp(term, "gi")
-                            const isMatch = text.toLowerCase().split(pattern).length > 1;
+                        //stop the iteration if we find a match
+                        return !isTermFound
+                    })
 
-                            if (isMatch) {
-                                isTermFound = true;
-                                return;
-                            }
-                        })
+                    return isTermFound;
+                }
 
-                        return isTermFound;
-                    }
+                const isMatch = checkForTerms(text, rule.searchTerms);
 
-                    const isMatch = checkForTerms(text, rule.searchTerms);
+                if (isMatch) {
+                    card.remove();
+                    return false;
+                }
 
-                    if (isMatch) {
-                        card.remove();
-                        return;
-                    }
+                rule?.subPages?.linksToFollowSelectors?.forEach(linkToFollow => {
 
-                    rule?.subPages?.linksToFollowSelectors?.forEach(linkToFollow => {
+                    const link = card.querySelector(linkToFollow)?.href;
 
-                        const link = card.querySelector(linkToFollow).href;
+                    (async () => {
 
-                        (async () => {
-
+                        try {
                             const res = await (await fetch(link)).text();
 
                             const doc = document.createElement('html');
                             doc.innerHTML = res;
 
                             rule?.subPages?.itemsSelectors?.forEach(container => {
-                                const blocks = doc.getElementsByTagName(container);
+                                const blocks = doc.querySelector(container);
 
-                                blocks.forEach(block => {
+                                blocks.every(block => {
                                     const text = block.text;
 
-                                    const isMatch = checkForTerms(text, rule.subPages.searchTerms)
+                                    const isMatch = checkForTerms(text, rule?.subPages?.searchTerms)
 
                                     if (isMatch) {
                                         block.remove();
-                                        return;
+                                        return false;
                                     }
+
+                                    return true;
                                 })
 
                             })
-
-                        })();
-
-                    })
+                        }
+                        catch (err) {
+                            console.error(`Something went wrong trying to check content of a subpage (${link})`);
+                        }
+                    })();
 
                 })
+
+                return true;
             })
         })
-    }
+    })
 })
 
 
-// TODO: ON request from popup.js, allow the user to hover over elements and click to select which element they want ids calculated for!
+// TODO: ON request from popup.js, allow the user to hover over elements and click to select which element they want ids/css-selectors calculated for!
 function EditMode() {
     document.body.addEventListener('mouseover', function mouseoverListener(e) {
         const el = e.target;
-        const elementStyle = el.style;
         const computedStyle = window.getComputedStyle(el, null);
 
-        const inlineBackgroundColor = elementStyle['background-color'];
-        const inlineTextColor = elementStyle['color'];
         const computedBackgroundColor = computedStyle['background-color'];
         const computedTextColor = computedStyle['color'];
 
